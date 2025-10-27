87cf6e80e6bd405e6e79677a9d60c17ebdd520ff
Yaroslav Oliinyk <yaroslav.oliinyk.lviv@gmail.com>
2025-10-27 20:07:35 +0200

    Migration to Compose UI:  - BookDetailsContent: used Colum instead of LazyColumn

--- app/src/main/java/com/oliinyk/yaroslav/easyreads/ui/screen/book/details/components/BookDetailsContent.kt
+++ app/src/main/java/com/oliinyk/yaroslav/easyreads/ui/screen/book/details/components/BookDetailsContent.kt
@@ -2,11 +2,11 @@ package com.oliinyk.yaroslav.easyreads.ui.screen.book.details.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,51 +20,34 @@ fun BookDetailsContent(
     onEvent: (BookDetailsEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
+    Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(MaterialTheme.colorScheme.background)
-            .padding(horizontal = Dimens.paddingHorizontalMedium),
+            .padding(horizontal = Dimens.paddingHorizontalMedium, vertical = Dimens.spacerHeightMedium),
         verticalArrangement = Arrangement.spacedBy(Dimens.arrangementVerticalSpaceSmall)
     ) {
-        item {
-            Spacer(Modifier.height(Dimens.spacerHeightSmall))
-        }
-        item {
-            BookDetailsCoverSection(book = stateUi.book)
-        }
-        item {
-            BookDetailsProgressSection(
-                stateUi = stateUi,
-                onStartReadingSession = { onEvent(BookDetailsEvent.StartReadingSession) },
-                onSeeAll = { onEvent(BookDetailsEvent.SeeAllReadingSessions) }
-            )
-        }
+        BookDetailsCoverSection(book = stateUi.book)
+        BookDetailsProgressSection(
+            stateUi = stateUi,
+            onStartReadingSession = { onEvent(BookDetailsEvent.StartReadingSession) },
+            onSeeAll = { onEvent(BookDetailsEvent.SeeAllReadingSessions) }
+        )
         if (!stateUi.book.isFinished) {
-            item {
-                BookDetailsReadingSessionsSection(
-                    sessions = stateUi.readingSessions,
-                    onSeeAll = { onEvent(BookDetailsEvent.SeeAllReadingSessions) },
-                    onEdit = { onEvent(BookDetailsEvent.EditReadingSession(it)) }
-                )
-            }
-        }
-        item {
-            BookDetailsNotesSection(
-                notes = stateUi.notes,
-                onSeeAllNotes = { onEvent(BookDetailsEvent.SeeAllNotes) },
-                onAddNote = { onEvent(BookDetailsEvent.AddNote(it)) },
-                onEditNote = { onEvent(BookDetailsEvent.EditNote(it)) }
+            BookDetailsReadingSessionsSection(
+                sessions = stateUi.readingSessions,
+                onSeeAll = { onEvent(BookDetailsEvent.SeeAllReadingSessions) },
+                onEdit = { onEvent(BookDetailsEvent.EditReadingSession(it)) }
             )
         }
-        item {
-            BookDetailsIsbnSection(isbn = stateUi.book.isbn)
-        }
-        item {
-            BookDetailsDescriptionSection(stateUi.book.description)
-        }
-        item {
-            Spacer(Modifier.height(Dimens.spacerHeightSmall))
-        }
+        BookDetailsNotesSection(
+            notes = stateUi.notes,
+            onSeeAllNotes = { onEvent(BookDetailsEvent.SeeAllNotes) },
+            onAddNote = { onEvent(BookDetailsEvent.AddNote(it)) },
+            onEditNote = { onEvent(BookDetailsEvent.EditNote(it)) }
+        )
+        BookDetailsIsbnSection(isbn = stateUi.book.isbn)
+        BookDetailsDescriptionSection(stateUi.book.description)
     }
 }
