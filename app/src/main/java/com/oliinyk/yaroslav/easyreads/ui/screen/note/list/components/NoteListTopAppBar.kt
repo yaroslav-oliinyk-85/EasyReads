c049bf0f0df3b8cf9398a811e0672cd7883a1360
Yaroslav Oliinyk <yaroslav.oliinyk.85@gmail.com>
2025-11-24 19:25:31 +0200

    Design:  - Added AppBadge to NoteListTopScreen

--- app/src/main/java/com/oliinyk/yaroslav/easyreads/ui/screen/note/list/components/NoteListTopAppBar.kt
+++ app/src/main/java/com/oliinyk/yaroslav/easyreads/ui/screen/note/list/components/NoteListTopAppBar.kt
@@ -1,5 +1,8 @@
 package com.oliinyk.yaroslav.easyreads.ui.screen.note.list.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,9 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.oliinyk.yaroslav.easyreads.R
+import com.oliinyk.yaroslav.easyreads.ui.components.AppBadge
 import com.oliinyk.yaroslav.easyreads.ui.components.AppIconTopBarButton
+import com.oliinyk.yaroslav.easyreads.ui.theme.Dimens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,11 +28,20 @@ fun NoteListTopAppBar(
 ) {
     TopAppBar(
         title = {
-            Text(
-                stringResource(
-                    R.string.note_list__toolbar__title_text,
-                    noteSize)
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    stringResource(
+                        R.string.note_list__toolbar__title_text
+                    )
+                )
+                Spacer(Modifier.width(Dimens.spacerWidthSmall))
+                AppBadge(
+                    text = noteSize.toString(),
+                    borderColor = Color.Unspecified
+                )
+            }
         },
         actions = {
             AppIconTopBarButton(
