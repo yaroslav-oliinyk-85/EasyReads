4af72aa3367841ca700aea3adce67efc79607035
Yaroslav Oliinyk <yaroslav.oliinyk.85@gmail.com>
2025-11-24 19:23:11 +0200

    Design:  - Added AppBadge to ReadingSessionListScreen

--- app/src/main/java/com/oliinyk/yaroslav/easyreads/ui/screen/reading_session/list/components/ReadingSessionListTopAppBar.kt
+++ app/src/main/java/com/oliinyk/yaroslav/easyreads/ui/screen/reading_session/list/components/ReadingSessionListTopAppBar.kt
@@ -1,13 +1,21 @@
 package com.oliinyk.yaroslav.easyreads.ui.screen.reading_session.list.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.oliinyk.yaroslav.easyreads.R
+import com.oliinyk.yaroslav.easyreads.ui.components.AppBadge
+import com.oliinyk.yaroslav.easyreads.ui.theme.Dimens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -16,12 +24,20 @@ fun ReadingSessionListTopAppBar(
 ) {
     TopAppBar(
         title = {
-            Text(
-                text = stringResource(
-                    R.string.reading_session_list__toolbar__title_text,
-                    sessionsCount
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(
+                        R.string.reading_session_list__toolbar__title_text
+                    )
                 )
-            )
+                Spacer(Modifier.width(Dimens.spacerWidthSmall))
+                AppBadge(
+                    text = sessionsCount.toString(),
+                    borderColor = Color.Unspecified
+                )
+            }
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary,
